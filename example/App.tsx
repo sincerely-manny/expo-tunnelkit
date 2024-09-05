26f4dcfdd0665aff8c62cfcd70fb16de7a34429e
Kirill Serebryannyy <sincerely.manny@gmail.com>
2024-09-05 21:37:08 +0300

    feat: Add example app

--- example/App.tsx
+++ example/App.tsx
@@ -1,20 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ExpoTunnelkit } from 'expo-tunnelkit';
+import { VpnStatus } from 'expo-tunnelkit/ExpoTunnelkit.types';
+import { useCallback, useEffect, useState } from 'react';
+import { Button, Text, View } from 'react-native';
 
-import * as ExpoTunnelkit from 'expo-tunnelkit';
+import { ovpnConfig } from './ovpn-config';
 
 export default function App() {
+  const [status, setStatus] = useState<VpnStatus>('Unknown');
+  const [ready, setReady] = useState(false);
+
+  const init = useCallback(async () => {
+    ExpoTunnelkit.setup(
+      'group.com.appstrain.ahvpn.VpnProvider',
+      'com.appstrain.ahvpn.VpnProvider',
+    );
+    ExpoTunnelkit.setCredentials('freeopenvpn', '127461219');
+    await ExpoTunnelkit.configFromString(ovpnConfig);
+    setReady(true);
+  }, []);
+
+  useEffect(() => {
+    const subscription = ExpoTunnelkit.addVpnStatusListener((status) => {
+      console.log('Current status', status.VPNStatus);
+      setStatus(status.VPNStatus);
+    });
+    init();
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>{ExpoTunnelkit.hello()}</Text>
+    <View>
+      <Text>Home</Text>
+      <View
+        style={{
+          opacity: ready ? 1 : 0.5,
+          flexDirection: 'row',
+          gap: 8,
+        }}
+      >
+        <Button
+          title="Connect"
+          onPress={ExpoTunnelkit.connect}
+          disabled={!ready}
+        />
+        <Button
+          title="Disconnect"
+          onPress={ExpoTunnelkit.disconnect}
+          disabled={!ready}
+        />
+      </View>
+      <View>
+        <Text>
+          {status === 'Connected' ? '🟢' : '🔴'} Status: {status}
+        </Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
